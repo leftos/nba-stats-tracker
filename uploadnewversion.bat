f6a5afc171832cb9e7b53873a6b916c1f19c3a6f
Leftos <leftos@gmail.com>
2013-02-08 17:39:36 +0200

    fix .gitignore'd files

--- uploadnewversion.bat
+++
@@ -1,3 +0,0 @@
-"E:\Program Files (x86)\WinSCP\winscp.com" /command "option batch abort" "option confirm off" "open diogenis" "put ""E:\Development\Visual Studio 2010\Projects\NBA Stats Tracker\nstversion.txt"" " "exit"
-"E:\Program Files (x86)\WinSCP\winscp.com" /command "option batch abort" "option confirm off" "open diogenis" "put ""E:\Development\Visual Studio 2010\Projects\NBA Stats Tracker\NBA Stats Tracker\What's New.txt"" nstchangelog.txt" "exit"
-"E:\Program Files (x86)\WinSCP\winscp.com" /command "option batch abort" "option confirm off" "open nlsc" "put ""E:\Development\Visual Studio 2010\Projects\NBA Stats Tracker\NBA Stats Tracker.exe"" " "exit"
