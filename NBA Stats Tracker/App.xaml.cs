0c6e19259692dfc320ae0a1f57433ac4caa9fc65
Leftos <leftos@gmail.com>
2013-02-12 21:21:17 +0200

    Improved Unhandled Exception unhandler

--- NBA Stats Tracker/App.xaml.cs
+++ NBA Stats Tracker/App.xaml.cs
@@ -52,29 +52,42 @@ namespace NBA_Stats_Tracker
         /// </param>
         private void app_DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
         {
+            var exceptionString = e.Exception.ToString();
+            var innerExceptionString = e.Exception.InnerException == null
+                                           ? "No inner exception information."
+                                           : e.Exception.InnerException.Message;
+            var versionString = "Version " + Assembly.GetExecutingAssembly().GetName().Version;
+
             try
             {
-                var f = new StreamWriter(NBA_Stats_Tracker.Windows.MainWindow.AppDocsPath + @"\errorlog_unh.txt");
+                var errorReportPath = NBA_Stats_Tracker.Windows.MainWindow.AppDocsPath + @"errorlog_unh.txt";
+                var f = new StreamWriter(errorReportPath);
 
                 f.WriteLine("Unhandled Exception Error Report for NBA Stats Tracker");
-                f.WriteLine("Version " + Assembly.GetExecutingAssembly().GetName().Version);
+                f.WriteLine(versionString);
                 f.WriteLine();
-                f.Write(e.Exception.ToString());
-                f.WriteLine();
-                f.WriteLine();
-                f.Write(e.Exception.InnerException == null ? "None" : e.Exception.InnerException.Message);
+                f.WriteLine("Exception information:");
+                f.Write(exceptionString);
                 f.WriteLine();
                 f.WriteLine();
+                f.WriteLine("Inner Exception information:");
+                f.Write(innerExceptionString);
                 f.Close(); 
 
                 MessageBox.Show("NBA Stats Tracker encountered a critical error and will be terminated.\n\nAn Error Log has been saved at " +
-                             NBA_Stats_Tracker.Windows.MainWindow.AppDocsPath + @"\errorlog_unh.txt");
+                             errorReportPath, "NBA Stats Tracker Error", MessageBoxButton.OK, MessageBoxImage.Error);
 
-                Process.Start(NBA_Stats_Tracker.Windows.MainWindow.AppDocsPath + @"\errorlog_unh.txt");
+                Process.Start(errorReportPath);
             }
             catch (Exception ex)
             {
-                MessageBox.Show("Can't create errorlog!\n\n" + ex + "\n\n" + ex.InnerException);
+                string s = "Can't create errorlog!\nException: " + ex;
+                s += ex.InnerException != null ? "\nInner Exception: " + ex.InnerException : "";
+                s += "\n\n";
+                s += versionString;
+                s += "Exception Information:\n" + exceptionString + "\n\n";
+                s += "Inner Exception Information:\n" + innerExceptionString;
+                MessageBox.Show(s, "NBA Stats Tracker Error", MessageBoxButton.OK, MessageBoxImage.Error);
             }
 
             // Prevent default unhandled exception processing
