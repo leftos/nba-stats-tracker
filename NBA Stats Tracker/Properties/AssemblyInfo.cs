23b53e7501d9f4b16209d4d66c9dba992bab35b6
Leftos <leftos@gmail.com>
2013-05-25 00:08:32 +0300

    (v1.14.1) Updated version information

--- NBA Stats Tracker/Properties/AssemblyInfo.cs
+++ NBA Stats Tracker/Properties/AssemblyInfo.cs
@@ -75,4 +75,4 @@ using System.Windows;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.14.0.1")]
+[assembly: AssemblyVersion("1.14.1.0")]
